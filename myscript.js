5ae4ed8463ce44fa61e26b5a5ecee4cad6791ce6
mmadhuparna <mmadhuparna@yahoo.com>
2018-08-08 14:05:09 +0530

    commit

---
+++ myscript.js
@@ -0,0 +1,28 @@
+//Declaring And Initializing variables
+
+var recipeName ="Creamy squash linguine";
+var preparationTime = 45;
+var recipeBy = "Jimmy Cutter";
+var isVegetarian = true;
+var calories = 223.7;
+
+//log variable values into console
+console.log('Recipe Details After Update');
+console.log('--------------');
+console.log('Recipe Name:'+ recipeName);
+console.log('Preparation Time:'+ preparationTime);
+console.log('Recipe By:'+ recipeBy);
+console.log('Is Vegetarian Dish:'+ isVegetarian);
+console.log('Calories:'+ calories);
+
+//Re-assigning variables
+preparationTime = 60;
+
+//log Updated variable values into console 
+console.log("Recipe Details");
+console.log('--------------');
+console.log('Recipe Name:'+ recipeName);
+console.log('Preparation Time:'+ preparationTime);
+console.log('Recipe By:'+ recipeBy);
+console.log('Is Vegetarian Dish:'+ isVegetarian);
+console.log('Calories:'+ calories);
